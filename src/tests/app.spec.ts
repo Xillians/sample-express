20ff9ffbfedb7900abe30c7cef58edf8b7bb7109
Xillians <chkxillians@gmail.com>
2021-03-09 12:15:11 +0100

    Something in noResponse made test never stop

--- src/tests/app.spec.ts
+++ src/tests/app.spec.ts
@@ -83,13 +83,5 @@ describe("testing application endpoints", () =>{
                     done();
                 });
         });
-        it("/noResponse", done => {
-            chai.request(app)
-                .get('/noResponse')
-                .end( (err, res) => {
-                    expect(res, "Response is unexpectedly defined").to.be.undefined;
-                    done();
-                });
-        });
     });
 });
