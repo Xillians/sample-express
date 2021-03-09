db6d7e79de817b896dda362100bcd41ef4c26855
Xillians <chkxillians@gmail.com>
2021-03-09 09:54:57 +0100

    set up test setup for chai

---
+++ src/app.spec.ts
@@ -0,0 +1,12 @@
+import assert from 'assert';
+import { environment } from './controller';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from './app';
+
+describe("testing application endpoints", () =>{
+    describe("endpoints", () => {
+        it("crash", async done => {
+        });
+    });
+});
